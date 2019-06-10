03b0118a6a40bf7d781e7c81598aa7653dd089be
Michael Krøysert-Simsø <mikke.korgen@gmail.com>
2019-06-10 23:23:08 +0200

    Add form to join.html. Add form validation

--- js/join.js
+++ js/join.js
@@ -1,1 +1,28 @@
 console.log('ok');
+
+const joinForm = document.querySelector('.b-join__form');
+let joinInputs = joinForm.getElementsByTagName('input');
+
+document.addEventListener('click', function(e) {
+  if (e.target.classList.contains('b-join__form__submit')) {
+    if (joinForm.checkValidity()) {
+      joinForm.submit();
+    } else {
+      e.preventDefault();
+      let joinInputs = joinForm.querySelectorAll('input');
+      joinInputs.forEach(input => {
+        if (!input.checkValidity()) {
+          input.classList.add('invalid');
+        }
+      });
+    }
+  }
+});
+Array.from(joinInputs).forEach(input => {
+  input.addEventListener('blur', function(e) {
+    console.log('blur');
+    if (!e.target.checkValidity()) {
+      e.target.classList.add('invalid');
+    }
+  });
+});
